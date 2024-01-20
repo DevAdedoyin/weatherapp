b1c7e32cc9836868c075a54df82c116801ab9cea
Adedoyin <isradeoluwa@gmail.com>
2024-01-20 11:35:56 +0000

    goRouter provider created

--- lib/src/routing/go_router_provider.dart
+++ lib/src/routing/go_router_provider.dart
@@ -5,10 +5,14 @@ import 'package:weatherapp/src/routing/app_routes.dart';
 import 'package:weatherapp/src/routing/route_error_screen.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(initialLocation: '/', routes: [
-    GoRoute(
-        path: '/',
-        name: AppRoutes.root,
-        builder: (context, state) => const SplashScreen())
-  ], errorBuilder: (context, state) => RouteErrorScreen(errorMsg: state.error.toString()));
+  return GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(
+            path: '/',
+            name: AppRoutes.root,
+            builder: (context, state) => const SplashScreen())
+      ],
+      errorBuilder: (context, state) =>
+          RouteErrorScreen(errorMsg: state.error.toString()));
 });
