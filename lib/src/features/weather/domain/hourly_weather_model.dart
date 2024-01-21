c5b2e790465a4d01807b225cf4c9114909492235
Adedoyin <isradeoluwa@gmail.com>
2024-01-21 13:02:54 +0000

    Hourly Weather Model Created

---
+++ lib/src/features/weather/domain/hourly_weather_model.dart
@@ -0,0 +1,52 @@
+import 'package:weatherapp/src/features/weather/domain/weather_model.dart';
+
+class HourlyWeatherModel {
+  final int dateTime;
+  final double temp;
+  final double feelsLike;
+  final int pressure;
+  final int humidity;
+  final int dewPoint;
+  final double windSpeed;
+  final int windDegree;
+  final SubCurrentWeather weather;
+
+  // Constructor for HourlyWeatherModel
+  HourlyWeatherModel(
+      {required this.dateTime,
+      required this.temp,
+      required this.feelsLike,
+      required this.pressure,
+      required this.humidity,
+      required this.dewPoint,
+      required this.windSpeed,
+      required this.windDegree,
+      required this.weather});
+
+  // Factory method to create HourlyWeatherModel from JSON data
+  factory HourlyWeatherModel.fromJson(Map<String, dynamic> data) {
+    final dateTime = data["dt"] as int;
+    final temp = data["temp"] as double;
+    final feelsLike = data["feels_like"] as double;
+    final pressure = data["pressure"] as int;
+    final humidity = data["humidity"] as int;
+    final dewPoint = data["dew_point"] as int;
+    final windSpeed = data["wind_speed"] as double;
+    final windDegree = data["wind_deg"] as int;
+    final weatherData = data["weather"] as List<Map<String, dynamic>>;
+
+    // Use the first element of the weatherData list to create SubCurrentWeather
+    final weather = SubCurrentWeather.fromJson(weatherData);
+
+    return HourlyWeatherModel(
+        dateTime: dateTime,
+        temp: temp,
+        feelsLike: feelsLike,
+        pressure: pressure,
+        humidity: humidity,
+        dewPoint: dewPoint,
+        windSpeed: windSpeed,
+        windDegree: windDegree,
+        weather: weather);
+  }
+}
