e1213bd3159908f8ab8a4d39a5d125b5ec30c60b
Adedoyin <isradeoluwa@gmail.com>
2024-01-25 19:23:34 +0000

    google signout provider

--- lib/src/features/authentication/data/repositories/google_signout_repository.dart
+++ lib/src/features/authentication/data/repositories/google_signout_repository.dart
@@ -2,4 +2,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weatherapp/src/features/authentication/data/datasources/google_signout_datasource.dart';
 
 final googleSignoutProvider =
-    FutureProvider((ref) => ref.watch(googleSignOutProvider_));
+    FutureProvider((ref) async => ref.watch(googleSignOutProvider_));
