cd41b51e281aed4b45650c327347d7438f98de19
Adedoyin <isradeoluwa@gmail.com>
2024-02-03 23:08:32 +0000

    Onboarding

--- lib/src/features/onboarding/onboarding_screen.dart
+++ lib/src/features/onboarding/onboarding_screen.dart
@@ -6,6 +6,6 @@ class OnboardingScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(body: SizedBox());
+    return Scaffold(body: SizedBox(child: Pageview()));
   }
 }
