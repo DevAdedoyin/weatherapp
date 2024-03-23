4f2666a8087f015320b9be877ae559d90b6319e9
Adedoyin <isradeoluwa@gmail.com>
2024-03-23 14:08:45 +0000

    Search city textfield suggestion widget created and completed

--- lib/src/features/weather/presentation/dashboard_pages/search_page.dart
+++ lib/src/features/weather/presentation/dashboard_pages/search_page.dart
@@ -1,6 +1,10 @@
 import "package:field_suggestion/box_controller.dart";
+import "package:field_suggestion/field_suggestion.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:google_fonts/google_fonts.dart";
+import "package:weatherapp/src/common/gaps/sized_box.dart";
+import "package:weatherapp/src/features/weather/data/repositories/search_suggestion_data.dart";
 
 class SearchPage extends ConsumerStatefulWidget {
   const SearchPage({super.key});
@@ -10,7 +14,6 @@ class SearchPage extends ConsumerStatefulWidget {
 }
 
 class _SearchPageState extends ConsumerState<SearchPage> {
-
   // A box controller for default and local usable FieldSuggestion.
   final boxController = BoxController();
 
@@ -23,18 +26,110 @@ class _SearchPageState extends ConsumerState<SearchPage> {
   // A text editing controller for network usable FieldSuggestion.
   final textControllerNetwork = TextEditingController();
 
-  
+  // A fake future builder that waits for 1 second to complete search.
+  final strSuggestions = [
+    'Lagos',
+    'London',
+    'New York City',
+    'Beijing',
+    'São Paulo',
+    "Sydney"
+  ];
+  Future<List<String>> future(String input) => Future<List<String>>.delayed(
+        const Duration(seconds: 1),
+        () => strSuggestions
+            .where((s) => s.toLowerCase().contains(input.toLowerCase()))
+            .toList(),
+      );
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(children: [
-        SizedBox(
-          child: Row(children: [
-            TextField()
-          ],),
-        )
-      ],),
+      child: Container(
+        // height: 30,
+        child: Column(
+          children: [
+            verticalGap(10),
+            SizedBox(
+              width: size.width * 0.9,
+              child: FieldSuggestion<SearchSuggestionModel>(
+                itemBuilder: (_, position) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        textController.text = SearchSuggestionModel
+                            .listOfCityData[position].cityNames;
+                      });
+
+                      textController.selection = TextSelection.fromPosition(
+                        TextPosition(offset: textController.text.length),
+                      );
+                    },
+                    child: Card(
+                      child: ListTile(
+                        title: Text(SearchSuggestionModel
+                            .listOfCityData[position].cityNames),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.clear),
+                          onPressed: () {
+                            SearchSuggestionModel.listOfCityData
+                                .removeAt(position);
+                            boxController.refresh?.call();
+                          },
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                inputDecoration: InputDecoration(
+                    hintText: 'Search location name...',
+                    isCollapsed: true,
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 15),
+                    hintStyle: GoogleFonts.roboto(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 17,
+                      color: Colors.grey[500],
+                      fontStyle: FontStyle.italic,
+                    ),
+                    suffixIcon: Container(
+                        width: 4,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 0.5,
+                              height: 25,
+                              color: Colors.grey,
+                            ),
+                            // horizontalGap(2),
+                            Icon(Icons.arrow_forward),
+                          ],
+                        )),
+                    prefixIcon: SizedBox(
+                        child: Icon(
+                      Icons.search,
+                    ))),
+                textController: textController,
+                suggestions: SearchSuggestionModel.listOfCityData,
+                boxController: boxController,
+                separatorBuilder: (_, __) => const Divider(),
+                search: (item, input) {
+                  // Disable box, if item selected.
+                  if (item.cityNames == input) return false;
+
+                  return item.cityNames
+                      .toString()
+                      .toLowerCase()
+                      .contains(input.toLowerCase());
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
