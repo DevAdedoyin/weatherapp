2727a6908c94af1e8521c9bc0e9768baa4940799
Adedoyin <isradeoluwa@gmail.com>
2024-03-23 12:18:06 +0000

    Search city model and data list created

--- lib/src/features/weather/data/repositories/search_suggestion_data.dart
+++ lib/src/features/weather/data/repositories/search_suggestion_data.dart
@@ -1,5 +1,79 @@
 class SearchCityData {
-  final cityNames;
+  final String cityNames;
 
   SearchCityData({required this.cityNames});
+
+  static List<SearchCityData> listOfCityData = [
+    // AFRICA
+    SearchCityData(cityNames: "Cairo"),
+    SearchCityData(cityNames: "Johannesburg"),
+    SearchCityData(cityNames: "Lagos"),
+    SearchCityData(cityNames: "Nairobi"),
+    SearchCityData(cityNames: "Casablanca"),
+    SearchCityData(cityNames: "Cape Town"),
+    SearchCityData(cityNames: "Accra"),
+    SearchCityData(cityNames: "Marrakech"),
+    SearchCityData(cityNames: "Tunis"),
+    SearchCityData(cityNames: "Dar es Salaam"),
+
+    // ASIA
+    SearchCityData(cityNames: "Tokyo"),
+    SearchCityData(cityNames: "Shanghai"),
+    SearchCityData(cityNames: "Delhi"),
+    SearchCityData(cityNames: "Seoul"),
+    SearchCityData(cityNames: "Beijing"),
+    SearchCityData(cityNames: "Mumbai"),
+    SearchCityData(cityNames: "Istanbul"),
+    SearchCityData(cityNames: "Bangkok"),
+    SearchCityData(cityNames: "Jakarta"),
+    SearchCityData(cityNames: "Dubai"),
+
+    // Europe
+    SearchCityData(cityNames: "London"),
+    SearchCityData(cityNames: "Paris"),
+    SearchCityData(cityNames: "Rome"),
+    SearchCityData(cityNames: "Istanbul"),
+    SearchCityData(cityNames: "Moscow"),
+    SearchCityData(cityNames: "Berlin"),
+    SearchCityData(cityNames: "Madrid"),
+    SearchCityData(cityNames: "Barcelona"),
+    SearchCityData(cityNames: "Amsterdam"),
+    SearchCityData(cityNames: "Vienna"),
+
+    // North America
+    SearchCityData(cityNames: "New York City"),
+    SearchCityData(cityNames: "Los Angeles"),
+    SearchCityData(cityNames: "Mexico City"),
+    SearchCityData(cityNames: "Toronto"),
+    SearchCityData(cityNames: "Chicago"),
+    SearchCityData(cityNames: "Miami"),
+    SearchCityData(cityNames: "Vancouver"),
+    SearchCityData(cityNames: "Houston"),
+    SearchCityData(cityNames: "Atlanta"),
+    SearchCityData(cityNames: "Montreal"),
+
+    // South America
+    SearchCityData(cityNames: "São Paulo"),
+    SearchCityData(cityNames: "Buenos Aires"),
+    SearchCityData(cityNames: "Rio de Janeiro"),
+    SearchCityData(cityNames: "Bogotá"),
+    SearchCityData(cityNames: "Lima"),
+    SearchCityData(cityNames: "Santiago"),
+    SearchCityData(cityNames: "Brasília"),
+    SearchCityData(cityNames: "Quito"),
+    SearchCityData(cityNames: "Caracas"),
+    SearchCityData(cityNames: "Montevideo"),
+
+    // Australia
+    SearchCityData(cityNames: "Sydney"),
+    SearchCityData(cityNames: "Melbourne"),
+    SearchCityData(cityNames: "Brisbane"),
+    SearchCityData(cityNames: "Perth"),
+    SearchCityData(cityNames: "Auckland"),
+    SearchCityData(cityNames: "Wellington"),
+    SearchCityData(cityNames: "Adelaide"),
+    SearchCityData(cityNames: "Gold Coast"),
+    SearchCityData(cityNames: "Christchurch"),
+    SearchCityData(cityNames: "Canberra"),
+  ];
 }
