04b6c4043066fdaed78164fecab4f807d90c41ae
Adedoyin <isradeoluwa@gmail.com>
2024-04-01 10:30:02 +0100

    Setttings page created

--- lib/src/features/weather/presentation/dashboard_pages/settings_page.dart
+++ lib/src/features/weather/presentation/dashboard_pages/settings_page.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
+import "package:weatherapp/src/common/gaps/sized_box.dart";
+import "package:weatherapp/src/constants/app_colors.dart";
 
 class SettingsPage extends ConsumerStatefulWidget {
   const SettingsPage({super.key});
@@ -11,6 +14,82 @@ class SettingsPage extends ConsumerStatefulWidget {
 class _SettingsPageState extends ConsumerState<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          verticalGap(20),
+          Container(
+            child: const SizedBox(
+              child: Icon(Icons.person),
+            ),
+          ),
+          const Text("Adedoyin Oluwaleke"),
+          verticalGap(20),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: Card(
+              elevation: 7,
+              color: AppColors.scaffoldBgColor,
+              child: ListTile(
+                leading: const Icon(Icons.info),
+                title: const Text("About app"),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_forward_rounded),
+                ),
+              ),
+            ),
+          ),
+          verticalGap(10),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            child: Card(
+              elevation: 7,
+              color: AppColors.scaffoldBgColor,
+              child: ListTile(
+                leading: const Icon(Icons.lock),
+                title: const Text("Change password"),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_forward_rounded),
+                ),
+              ),
+            ),
+          ),
+          verticalGap(10),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: Card(
+              elevation: 7,
+              color: AppColors.scaffoldBgColor,
+              child: ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text("Logout"),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_forward_rounded),
+                ),
+              ),
+            ),
+          ),
+          verticalGap(10),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: Card(
+              elevation: 7,
+              color: AppColors.scaffoldBgColor,
+              child: ListTile(
+                leading: const Icon(Icons.delete),
+                title: const Text("Remove account"),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_forward_rounded),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
