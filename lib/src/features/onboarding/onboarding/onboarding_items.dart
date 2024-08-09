47aa80b70e2f45f509629ac8fef3c86465ab7cda
Adedoyin <isradeoluwa@gmail.com>
2024-08-09 22:10:52 +0100

    All fixes completed

--- lib/src/features/onboarding/onboarding/onboarding_items.dart
+++ lib/src/features/onboarding/onboarding/onboarding_items.dart
@@ -2,22 +2,22 @@ import 'package:weatherapp/src/features/onboarding/onboarding/onboarding_body.da
 
 List<OnboardingBodyWidget> onboardingItemsList = [
   const OnboardingBodyWidget(
-      image: "assets/images/onb0.jpg",
+      image: "assets/images/onboard1.png",
       title: "Authentication",
       description:
           "Register or Sign-in to gain access to all features of the app."),
   const OnboardingBodyWidget(
-      image: "assets/images/onb1.jpg",
+      image: "assets/images/onboard2.png",
       title: "Location",
       description:
           "Access to your current location to give you weather data, based on the location."),
   const OnboardingBodyWidget(
-      image: "assets/images/onb2.png",
+      image: "assets/images/onboard3.png",
       title: "Weather Forecast",
       description:
           "Enjoy real-time weather updates and forecasts for future hours and dates."),
   const OnboardingBodyWidget(
-      image: "assets/images/onb3.jpg",
+      image: "assets/images/onboard4.png",
       title: "Location Search",
       description: "Search for weather data for any location round the world."),
 ];
